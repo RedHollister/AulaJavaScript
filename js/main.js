32edcdc7ecd588185bcde99f1e35d249387f3c4f
Elton Guilherme <elton.guilherme@hotmail.com>
2020-11-24 19:03:02 -0300

    Mudanças da Aula de Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Elton Guilherme";
+var idade = 18;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(idade + idade2)
+//alert(nome + " Tem " + idade + " Anos");
+console.log(nome);
+console.log(idade * idade2);
+console.log(frase.toUpperCase());
