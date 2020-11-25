dcd95604deb3e08172716ccf56c4e0230982983a
Elton Guilherme <elton.guilherme@hotmail.com>
2020-11-24 22:58:08 -0300

    Mudanças da Aula de Loop/If/Date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-var frutas = [{nome:"Maçã", cor:"Vermelha"}, {nome:"Uva", cor:"Roxa"}]
-console.log(frutas);
-alert(frutas[1].cor);
+var d = new Date();
+alert(d.getMinutes());
+
+
+/*
+
+var count;
+for (count = 0;count<=5; count++){
+    alert(count);
+}
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+}
+
+if (idade >= 18){
+    alert("Maior de idade");
+} else{
+    alert("Menor de Idade");
+}
+*/
+//var frutas = [{nome:"Maçã", cor:"Vermelha"}, {nome:"Uva", cor:"Roxa"}]
+//console.log(frutas);
+//alert(frutas[1].cor);
 
 //var fruta = {nome:"Maçã", cor:"Vermelha"}
 //console.log(fruta.nome);
@@ -11,4 +35,4 @@ alert(frutas[1].cor);
 //console.log(lista);
 //lista.pop();
 //console.log(lista.join(" e "));
-//alert(lista[0])
+//alert(lista[0]
